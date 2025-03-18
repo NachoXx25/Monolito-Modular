9e6b8641f2081b715f74214ea8e63c2b4499f507
NachoXx25 <ignacio.alfonso2002@gmail.com>
2025-03-18 15:34:21 -0300

    fix: resolve the dependencies conflict

--- obj/Debug/net9.0/Monolito-Modular.AssemblyInfo.cs
+++ obj/Debug/net9.0/Monolito-Modular.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Monolito-Modular")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f9d156fd7c3a33e9abd4af9229a28224e0d5bbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2654ab46d8af0425e5b3322e350055a9a95e989")]
 [assembly: System.Reflection.AssemblyProductAttribute("Monolito-Modular")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Monolito-Modular")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
