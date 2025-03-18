9fe768069a0ccb00191033baeeaa3d91a63f4e20
NachoXx25 <ignacio.alfonso2002@gmail.com>
2025-03-18 15:35:37 -0300

    feat: add and configure swagger

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
@@ -11,9 +12,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI();
+    app.UseSwagger();
 }
 
 
 app.UseHttpsRedirection();
+app.UseSwaggerUI();
+app.UseSwagger();
 app.Run();
 
