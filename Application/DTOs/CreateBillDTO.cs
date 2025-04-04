c102f4cf59f44f9a5fb5c6efc7f99e300538a25f
sebastian-nunez-g <sebastian.nunez@noasotonetworks.cl>
2025-04-04 17:23:28 -0300

    feat: add CreateBillDTO and CreatedBillDTO

--- Application/DTOs/CreateBillDTO.cs
+++ Application/DTOs/CreateBillDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,16 @@ namespace Monolito_Modular.Application.DTOs
 {
     public class CreateBillDTO
     {
+        [Required(ErrorMessage = "El id de usuario es requerido.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "El id de usuario debe ser un número entero positivo.")]
         public required int UserId { get; set; }
 
+        [Required(ErrorMessage = "El estado de la factura es requerido.")]
+        [RegularExpression(@"^(Pagado|Pendiente|Vencido)$", ErrorMessage = "El estado de la factura debe ser sólo 'Pagado', 'Pendiente' o 'Vencido'.")]
         public required string StatusName { get; set; }
 
+        [Required(ErrorMessage = "El monto a pagar es requerido.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "El monto a pagar debe ser un número entero positivo.")]
         public required int AmountToPay { get; set; }
     }
 }
