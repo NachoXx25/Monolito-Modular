1355ccb0ca1cc11a9c67c8919a548e80a89c0710
sebastian-nunez-g <sebastian.nunez@noasotonetworks.cl>
2025-04-05 14:53:19 -0300

    feat: uncomment the authorizations and add the statusName query param on the getBillsByUser

--- Api/Controllers/BillController.cs
+++ Api/Controllers/BillController.cs
@@ -14,7 +14,6 @@ namespace Monolito_Modular.Api.Controllers
     [Route("facturas")]
     public class BillController : ControllerBase
     {
-
         private readonly IBillService _billService;
         
         public BillController(IBillService billService)
@@ -22,8 +21,12 @@ namespace Monolito_Modular.Api.Controllers
             _billService = billService;
         }
 
+        /// <summary>
+        /// Endpoint para crear una nueva factura.
+        /// </summary>
+        /// <param name="bill">La nueva factura a agregar</param>
         [HttpPost]
-        //[Authorize(Roles = "Administrador")]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> CreateBill([FromBody] CreateBillDTO bill)
         {
             try{
@@ -39,8 +42,12 @@ namespace Monolito_Modular.Api.Controllers
             }
         }
 
+        /// <summary>
+        /// Endpoint que obtiene una factura por su id.
+        /// </summary>
+        /// <param name="id">El id de la factura a obtener</param>
         [HttpGet("{id}")]
-        //[Authorize]
+        [Authorize]
         public async Task<IActionResult> GetBillById(int id)
         {
             try
@@ -71,33 +78,49 @@ namespace Monolito_Modular.Api.Controllers
             }
         }
 
+        /// <summary>
+        /// Endpoint que obtiene todas las facturas de un usuario.
+        /// </summary>
+        /// <param name="statusName">Filtro opcional de estado de las facturas</param>
         [HttpGet]
-        //[Authorize]
-        public async Task<IActionResult> GetBillsByUser()
+        [Authorize]
+        public async Task<IActionResult> GetBillsByUser([FromQuery] string? statusName)
         {
-            var userId = User.FindFirstValue("Id");
-            var userRole = User.FindFirstValue(ClaimTypes.Role);
+            try
+                {
+                    var userId = User.FindFirstValue("Id");
+                var userRole = User.FindFirstValue(ClaimTypes.Role);
 
-            if (string.IsNullOrEmpty(userId))
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return BadRequest(new { Error = "El id de usuario no existe" });
+                }
+                if(string.IsNullOrEmpty(userRole)){
+                    return BadRequest(new { Error = "El rol de usuario no existe" });
+                }
+
+                var bills = await _billService.GetBills(int.Parse(userId), userRole, statusName);
+
+                if (bills == null)
+                {
+                    return NotFound(new { Error = "No se encontraron facturas" });
+                }
+
+                return Ok(bills);
+            }
+            catch (Exception ex)
             {
-                return BadRequest(new { Error = "El id de usuario no existe" });
+                return BadRequest(new { Error = ex.Message });
             }
-            if(string.IsNullOrEmpty(userRole)){
-                return BadRequest(new { Error = "El rol de usuario no existe" });
-            }
-
-            var bills = await _billService.GetBills(int.Parse(userId), userRole);
-
-            if (bills == null || bills.Length == 0)
-            {
-                return NotFound(new { Error = "No se encontraron facturas" });
-            }
-
-            return Ok(bills);
         }
 
+        /// <summary>
+        /// Endpoint para actualizar el estado de una factura.
+        /// </summary>
+        /// <param name="id">El id de la factura a actualizar</param>
+        /// <param name="statusName">El nuevo estado para la factura</param>
         [HttpPatch("{id}")]
-        //[Authorize(Roles = "Administrador")]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> UpdateBill(int id, [FromBody] string statusName)
         {
            try{
@@ -113,8 +136,13 @@ namespace Monolito_Modular.Api.Controllers
            }
         }
 
+        /// <summary>
+        /// Endpoint para eliminar lógicamente una factura.
+        /// </summary>
+        /// <param name="id">El id de la factura a borrar</param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
-        //[Authorize(Roles = "Administrador")]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> DeleteBill(int id)
         {
             try{
